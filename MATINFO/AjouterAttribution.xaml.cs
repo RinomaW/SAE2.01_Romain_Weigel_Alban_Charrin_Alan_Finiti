106570797826daa5ad58387b93fe5247c7011085
alan finiti <alan.finiti@etu.univ-smb.Fr>
2023-06-14 09:30:40 +0200

    caca

---
+++ MATINFO/AjouterAttribution.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MATINFO
+{
+    /// <summary>
+    /// Logique d'interaction pour AjouterAttribution.xaml
+    /// </summary>
+    public partial class AjouterAttribution : Window
+    {
+        public AjouterAttribution()
+        {
+            InitializeComponent();
+        }
+
+        private void TBComm_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.TBComm.Text = "";
+        }
+    }
+}
