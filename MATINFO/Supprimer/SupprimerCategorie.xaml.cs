3115bf2818385e5decd76e60f72221bd7c1987c5
alan finiti <alan.finiti@etu.univ-smb.Fr>
2023-06-16 09:57:30 +0200

    gregrdg

---
+++ MATINFO/Supprimer/SupprimerCategorie.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MATINFO.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MATINFO
+{
+    /// <summary>
+    /// Logique d'interaction pour AjouterCategorie.xaml
+    /// </summary>
+    public partial class SupprimerCategorie : Window
+    {
+        public SupprimerCategorie()
+        {
+            InitializeComponent();
+        }
+
+        string texte;
+
+        private void TBNomCate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            texte = this.TBNomCate.Text;
+        }
+
+        private void TBNomCate_GotFocus(object sender, RoutedEventArgs e)
+        {
+            this.TBNomCate.Text = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        
+    }
+}
