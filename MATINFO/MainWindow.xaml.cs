03ca79f670017ba56dcac23f60a3259a28a06314
Alban Charrin <Alban.Charrin@etu.univ-smb.fr>
2023-06-19 14:44:41 +0200

    seghesdc<egege<gg

--- MATINFO/MainWindow.xaml.cs
+++ MATINFO/MainWindow.xaml.cs
@@ -109,7 +109,40 @@ namespace MATINFO
             lvMateriel.Items.Refresh();
         }
 
-        private void lvCategorie_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        private void lvCategorie_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                ListBox listBox = sender as ListBox;
+                var valid = e.AddedItems[0];
+                foreach (var item in new ArrayList(listBox.SelectedItems))
+                {
+                    if (item != valid)
+                    {
+                        listBox.SelectedItems.Remove(item);
+                    }
+                }
+            }
+        }
+
+        private void lvMateriel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                ListBox listBox = sender as ListBox;
+                var valid = e.AddedItems[0];
+                foreach (var item in new ArrayList(listBox.SelectedItems))
+                {
+                    if (item != valid)
+                    {
+                        listBox.SelectedItems.Remove(item);
+                    }
+                }
+            }
+        }
+
+
+        private void lvAttribution_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (e.AddedItems.Count > 0)
             {
